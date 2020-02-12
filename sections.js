b24e2dd174817c8e27aa097e78ac276879ac1e04
c0d3ph1l1c <kwl.c0d3r@gmail.com>
2020-02-12 18:01:47 +0800

    Getting Started in progress
     	new file:   .gitignore
     	new file:   package-lock.json
     	new file:   package.json
     	new file:   sections.js
     	new file:   src/01_GettingStarted-01.html
     	new file:   src/js/01_GettingStarted-01.js
     	new file:   webpack.config.js

---
+++ sections.js
@@ -0,0 +1,4 @@
+module.exports = [
+  '01_GettingStarted-01',
+  'index'
+];
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const sections = require('./sections.js');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {},
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name]-[contentHash].js'
+  },
+  devServer: {
+    index: `${sections[sections.length-1]}.html`,
+    filename: `js/${sections[sections.length-1]}.js` 
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.html$/i,
+        use: 'html-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: 'file-loader'
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  }
+};
+
+sections.forEach(section => {
+  module.exports.entry[section] = `./src/js/${section}.js`;
+  module.exports.plugins.push(new HTMLWebpackPlugin({
+    title: section,
+    filename: `${section}.html`,
+    template: `./src/${section}.html`,
+    chunks: `${section}`
+  }));
+});
